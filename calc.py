29951c6bc317a707582704f20f4d90d4422d654b
Lutho <luthoyrn@gmail.com>
2025-03-06 01:06:22 +0200

    Added regular expression token functions and test code to calc.py

--- calc.py
+++ calc.py
@@ -12,3 +12,42 @@ tokens = [
     'MULTIPLY',
     'EQUALS'
 ]
+
+#regular expressions
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_EQUALS = r'\='
+t_ignore = r' '
+
+
+#token functions
+def t_FLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = 'NAME'
+    return t
+
+def t_error(t):
+    print('Illegal characters!')
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+lexer.input("mlamuli = 1.5 + 2")
+
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
