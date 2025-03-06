282da3ea02d0b9b177ee2efbb3941ed2da6cb426
Lutho <luthoyrn@gmail.com>
2025-03-06 02:31:44 +0200

    Added support for parsing and evaluating mathematical expressions with variables and precedence rules.

--- calc.py
+++ calc.py
@@ -19,7 +19,7 @@ t_MINUS = r'\-'
 t_MULTIPLY = r'\*'
 t_DIVIDE = r'\/'
 t_EQUALS = r'\='
-t_ignore = r' '
+t_ignore = r' \t'
 
 
 #token functions
@@ -42,12 +42,79 @@ def t_error(t):
     print('Illegal characters!')
     t.lexer.skip(1)
 
+#Building the lexer
 lexer = lex.lex()
 
-lexer.input("mlamuli = 1.5 + 2")
+precedence = (
+    ('left', 'PLUS', 'MINUS'), #low precedence
+    ('left', 'MULTIPLY', 'DIVIDE'), #high precedence
+)
+# Parsing Rules
+def p_calc(p):
+    '''
+    calc : expression
+         | var_assign
+         | empty
+    '''
+    print(run(p[1]))
+
+def p_var_assign(p):
+    '''
+    var_assign : NAME EQUALS expression
+    '''
+    p[0]= ('=',p[1],p[3])
+    
+def p_expression(p):
+    '''
+    expression : expression MULTIPLY expression
+               | expression DIVIDE expression
+               | expression PLUS expression
+               | expression MINUS expression
+    '''
+    p[0] = (p[2],p[1],p[3]) #("+",10,("*",5,4))
+
+def p_expression_int_float(p):
+    '''
+    expression : INT 
+               | FLOAT
+    '''
+    p[0] = p[1]
+
+def p_expression_var(p):
+    '''
+    expression : NAME
+    '''
+    p[0] = ('var',p[1])
+
+def p_error(p):
+    print("Syntax error found!")
+
+def p_empty(p):
+    '''
+    empty :
+    '''
+    p[0]= None
+
+parser = yacc.yacc()
+
+def run(p):
+    if type(p)==tuple:
+        if p[0]=='+':
+            return run(p[1])+run(p[2])
+        elif p[0]=='-':
+            return run(p[1])-run(p[2])
+        elif p[0]=='*':
+            return run(p[1])*run(p[2])
+        elif p[0]=='/':
+            return run(p[1])/run(p[2])
+        elif p[0]=='=':
+            return (p[1],p[2],p[3])
+    else:
+        return p
 
 while True:
-    tok = lexer.token()
-    if not tok:
+    try:
+        s=input('calc> ')
+    except EOFError: #CTRLD on keyboard
         break
-    print(tok)
+    parser.parse(s)
